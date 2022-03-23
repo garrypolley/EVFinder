a13416fed2b1f75fbd3a0f94a42b872083d819d0
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2022-03-23 18:56:58 -0400

    Initial commit

---
+++ app/src/components/FilterDropdown.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-dd id="trim-dd" size="sm" variant="outline-primary" class="px-1">
+          <template #button-content>
+            {{ filterDisplayName }}
+            <span v-if="localFilterSelections.trimDesc.length > 0">
+              <b-badge variant="success">
+                {{ localFilterSelections.trimDesc.length }}
+              </b-badge>
+            </span>
+          </template>
+
+          <b-dropdown-form>
+            <b-form-checkbox
+              v-for="item in this.filterOptions.trimDesc" :key=item
+              :value="item"
+              v-model="localFilterSelections.trimDesc"
+              name="trim-description"
+              class="mb-3"
+              >
+              {{ item }}
+            </b-form-checkbox>
+          </b-dropdown-form>
+        </b-dd>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+
+
+export default {
+  props: {
+    filterDisplayName: {required: true, type: String},
+    inventoryKey: {required: true, type: String}
+  },
+}
+</script>
+
+<style>
+
+</style>
